804009931dbcd8e8c7b6ba65560225ed2a17c224
jhy <yon3685@naver.com>
2021-11-02 12:45:52 +0900

    stage directory

---
+++ 02_stage/02_data.tf
@@ -0,0 +1,26 @@
+module "stage" {
+  source        = "../01_module"
+
+  region        = "ap-northeast-2"
+  cidr          = "0.0.0.0/0"
+  cidr_main     = "192.168.0.0/16"
+  cidr_v6       = "::/0"
+  name          = "jhy"
+  avazone       = ["a", "c"]
+  key           = "tf-key1"
+  public_s      = ["192.168.0.0/24", "192.168.2.0/24"]
+  private_s     = ["192.168.1.0/24", "192.168.3.0/24"]
+  private_dbs   = ["192.168.4.0/24", "192.168.5.0/24"]
+  private_ip    = "192.168.0.11"
+  zero_port     = 0
+  under_port    = 0
+  ssh_port      = 22
+  http_port     = 80
+  mysql_port    = 3306
+  prot_http     = "http"
+  prot_icmp     = "icmp"
+  prot_ssh      = "ssh"
+  prot_tcp      = "tcp"
+  instance      = "t2.micro"
+  protocol_HTTP = "HTTP"
+}
